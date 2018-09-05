dc6eea2e2a5ac97f8fb95dc71368784c38a7aba8
Booq <contact@booq.fr>
2018-09-05 22:17:01 +0200

    end of session 12

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,43 @@ console.log("App.js is running");
 
 // JSX - Javascript XML
 
-var template = <h1>does this change ?</h1>;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is a awesome app !',
+    options: []
+};
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options' }</p>
+        <ol>
+            <li>item 1</li>
+            <li>item 2</li>
+        </ol>
+    </div>
+);
+
+
+
+var user = {
+    name: 'Marc',
+    age: '33',
+    location: 'Nantes'
+};
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{(user.name) ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age : {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
